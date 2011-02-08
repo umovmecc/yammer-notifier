47867be4d133522c82450941ad3296bd68b73eef
Daniel Wildt <dwildt@gmail.com>
2011-02-08 03:32:32 -0200

    adding notifyanner.rb file, with sample call to yammer4r

---
+++ notifyammer.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+# run this file using 
+# ruby -r rubygems notifyammer.rb
+
+require 'yammer4r'
+
+# to generate oauth.yml check template generator
+# https://github.com/jpatterson/yammer4r/blob/master/bin/yammer_create_oauth_yml.rb
+config_path = File.dirname(__FILE__) + '/oauth.yml'
+yammer = Yammer::Client.new(:config => config_path)
+
+# group_id is the number associated with a group, not its related slug. 
+yammer.message(:post, {:group_id => '12345', :body =>'sample message to be sent to yammer'});
+
